5d3ba64de142fbcb867246b2f1e1ad01b3de9853
thanujakmt <thanujakmsdr@gmail.com>
2024-07-19 15:49:38 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,44 @@
 
 import { useState, useEffect, useRef } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import axios from 'axios'
 import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
+import { LuLoader2 } from "react-icons/lu";
+import { RiLoaderLine } from "react-icons/ri";
+import { IoIosArrowForward } from "react-icons/io";
+
+const UpdateMapCenter = ({ position }) => {
+  const map = useMap();
+  useEffect(() => {
+    map.setView(position, map.getZoom());
+  }, [position, map]);
+  return null;
+};
 
 function App() {
 
   const [data, setData] = useState()
+  const [loading,setLoading] = useState(false)
   const inputIp = useRef()
 
   const fetchData = async () => {
     try {
       let ip = inputIp.current.value ? inputIp.current.value : '32.115.105.0'
-
-      const response2 = await axios.get('https://freeipapi.com/api/json/100.255.255.255')
-      const response2Data = response2.data
-
-      console.log(response2Data)
-      // const response = await axios.get(`https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_LJRNbaX3nQwN124gwI8sdc11DCVvV=${ip}`);
+     
+      const ipRegex = /^(\d{1,3}\.){3}\d{1,3}$/;
+      if (!ipRegex.test(ip)){
+        alert('Invalid IP Address');
+        return
+      }
+      setLoading(true)
       
-      // const responseData = await response.data
-      // setData(responseData)
-      // console.log(responseData)
+      const response2 = await axios.get(`https://freeipapi.com/api/json/${ip}`)
+      const response2Data = response2.data
+      setData(response2Data)
+      setLoading(false)
     } catch (e){
-      console.log(e)
+      console.log('Somethin went wrong')
     }
   }
 
@@ -40,38 +52,42 @@ function App() {
         <h1 className=' text-white font-[500] text-[2.1em]'>IP Address Tracker</h1>
         <div className="search_btn flex items-center justify-center shadow-lg">
           <input type="text" ref={inputIp} placeholder='Search for any IP address or domain' className=' px-5 md:w-[500px] h-14 rounded-l-[15px] outline-none md:text-[1.3em] cursor-pointer' />
-          <img src="/images/icon-arrow.svg" onClick={async (e) => fetchData()} className='bg-black py-5 px-5 rounded-r-[15px] cursor-pointer hover:bg-Very_Dark_Gray' alt="" />
+          <div onClick={async (e) => fetchData()} className='bg-black py-5 text-white text-[18px] px-5 rounded-r-[15px] cursor-pointer hover:bg-Very_Dark_Gray'>
+            
+          {loading?<LuLoader2 className=' animate-spin' />:<IoIosArrowForward   />}
+            </div> 
         </div>
       </div>
-      <div className='z-10 flex items-center justify-center font-font_rubin'>
-        <div className='md:flex-row flex flex-col items-center justify-center bg-white md:gap-10 gap-4 top-[210px] h-[430px] w-[320px] md:w-[750px] lg:w-[900px] 2xl:w-[1100px] md:h-[180px] px-10 shadow-2xl rounded-[15px] absolute'>
-          <div className='w-[250px] md:w-[250px] px-1 flex flex-col items-center md:items-left  md:h-[100px] md:border-Dark_Gray md:border-r'>
+      <div className='  flex items-center justify-center font-font_rubin '>
+        <div className='md:flex-row flex flex-col items-center justify-center  z-10 bg-white md:gap-10 gap-4 top-[210px] h-[300px] w-[320px] md:w-[750px] lg:w-[900px] 2xl:w-[1100px] md:h-[180px] px-10 shadow-2xl rounded-[15px] absolute'>
+          <div className= ' justify-evenly w-[250px] md:w-[250px] px-1 flex flex-col items-center md:items-left  md:h-[100px] md:border-Dark_Gray md:border-r'>
             <p className=' text-Dark_Gray text-[0.8em] font-[600] tracking-wide'>IP ADDRESS</p>
-            <p className=' font-[500] text-[1.6em] text-Very_Dark_Gray '>{data ? data.ip : 'Loader...'}</p>
+            <p className=' font-[500] text-[1.2em] md:text-[1.6em] text-Very_Dark_Gray '>{data ? data.ipAddress : <RiLoaderLine  className='animate-spin'/>}</p>
           </div>
-          <div className='w-[250px] md:w-[250px] px-1 flex flex-col items-center md:items-left  md:h-[100px] md:border-Dark_Gray md:border-r'>
+          <div className='justify-evenly  w-[250px] md:w-[250px] px-1 flex flex-col items-center md:items-left  md:h-[100px] md:border-Dark_Gray md:border-r'>
             <p className=' text-Dark_Gray text-[0.8em] font-[600] tracking-wide'>LOCATION</p>
-            <p className=' font-[500] text-[1.6em] text-Very_Dark_Gray text-center '>{`${data ? `${data.location.region}, ${data.location.country} ,${data.location.postalCode}` : 'Loader...'}`}</p>
+            <p className=' font-[500] text-[1.2em] md:text-[1.6em] text-Very_Dark_Gray text-center '>{data ? `${data.regionName}, ${data.countryCode} ,${data.zipCode}` : <RiLoaderLine  className='animate-spin'/>}</p>
           </div>
-          <div className='w-[250px] md:w-[250px] px-1 flex flex-col items-center md:items-left  md:h-[100px] md:border-Dark_Gray md:border-r'>
+          <div className='justify-evenly  w-[250px] md:w-[250px] px-1 flex flex-col items-center md:items-left  md:h-[100px] md:border-Dark_Gray md:border-r'>
             <p className=' text-Dark_Gray text-[0.8em] font-[600] tracking-wide'>TIMZONE</p>
-            <p className=' font-[500] text-[1.6em] text-Very_Dark_Gray '>{data ? data.location.timezone : 'Loader...'}</p>
+            <p className=' font-[500] text-[1.2em] md:text-[1.6em] text-Very_Dark_Gray '>{data ? data.timeZone : <RiLoaderLine  className='animate-spin'/>}</p>
           </div>
-          <div className='w-[250px] md:w-[250px] px-1 flex flex-col items-center md:items-left  md:h-[100px]'>
-            <p className=' text-Dark_Gray text-[0.8em] font-[600] tracking-wide'>ISP</p>
-            <p className=' font-[500] text-[1.6em] text-Very_Dark_Gray '> {data ? data.isp : 'Loader...'}</p>
+          <div className='justify-evenly  w-[250px] md:w-[250px] px-1 flex flex-col items-center md:items-left  md:h-[100px]'>
+            <p className=' text-Dark_Gray text-[0.8em] font-[600] tracking-wide'>Currency</p>
+            <p className=' font-[500] text-[1.2em] md:text-[1.6em] text-Very_Dark_Gray '> {data ? data.currency.name : <RiLoaderLine  className='animate-spin'/>}</p>
           </div>
         </div>
       </div>
-      <div className=' -z-10 h-[600px] w-full' >
-        <MapContainer center={data?[data.location.lat, data.location.lng]:[40.71427,-74.00597]} zoom={13} scrollWheelZoom={false} style={{ height: '100%', width: '100%' }}>
+      <div className=' h-[600px]  w-full' >
+        <MapContainer className=' z-0 ' center={data?[data.latitude, data.longitude]:[40.71427,-74.00597]} zoom={13} scrollWheelZoom={true} style={{ height: '100%', width: '100%' }}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={data?[data.location.lat, data.location.lng]:[40.71427,-74.00597]}>
+          <UpdateMapCenter position={data?[data.latitude, data.longitude]:[40.71427,-74.00597]} />
+          <Marker  position={data?[data.latitude, data.longitude]:[40.71427,-74.00597]}>
             <Popup>
-              {`${data ? `${data.location.region}, ${data.location.country} ,${data.location.postalCode}` : 'Loader...'}`}
+              {`${data ? `${data.regionName}, ${data.countryCode} ,${data.zipCode}` : <RiLoaderLine  className='animate-spin'/>}`}
             </Popup>
           </Marker>
         </MapContainer>
